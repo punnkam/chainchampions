3d182e19def456a273b4dc81d0321d231e4aa039
CodingCocho <raulujr@gmail.com>
2022-01-15 14:23:11 -0800

    Navlink active state added and arena skeleton done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,31 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useRef } from 'react'
+import { NavLink } from 'react-router-dom'
 import './Navbar.css'
 
+
 export default function Navbar() {
+	const activeStyle = 
+	{
+		color: "gray"
+	};
+
+	const linkRef = useRef();
+	console.log(linkRef.current);
+
 	return (
 		<div className='Navbar'>
 			<div className='container'>
-				<Link className='title-link' to='/'>Chain Champions</Link>
+				<NavLink className='title-link' to='/'>Chain Champions</NavLink>
 				<div className='link-container'>
-					<Link className='link' to='/'>Home</Link>
-					<Link className='link' to='/arena'>Arena</Link>
-					<Link className='link' to='/whitepaper'>Whitepaper</Link>
-					<Link className='link' to='/staking'>Staking</Link>
+					<NavLink className='link' style={({isActive}) =>
+				isActive ? activeStyle : undefined} to='/'>Home</NavLink>
+					<NavLink className='link' style={({isActive}) =>
+				isActive ? activeStyle : undefined} to='/arena'>Arena</NavLink>
+					<NavLink className='link' style={({isActive}) =>
+				isActive ? activeStyle : undefined} to='/whitepaper'>Whitepaper</NavLink>
+					<NavLink className='link' style={({isActive}) =>
+				isActive ? activeStyle : undefined} to='/staking'>Staking</NavLink>
 				</div>
 			</div>
 		</div>
--- src/pages/Arena.js
+++ src/pages/Arena.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import './Arena.css'
 import Navbar from '../components/Navbar'
+import Champ from '../images/long_metal_sword_or_blue_steel.png'
 export default function Arena() {
     return (
         <div className='Arena'>
             <Navbar></Navbar>
+            <div className='main-container'>
+                <div className='arena-container-1'>
+                    <div className='user-container'>
+                        <div className='row-1'>
+                            <p>Your champs</p>
+                            <div className='champs-container'>
+                                <img src={Champ} alt='champ'></img>
+                                <img src={Champ} alt='champ'></img>
+                                <img src={Champ} alt='champ'></img>
+                            </div>
+                        </div>
+                        <div className='row-2'>
+
+                        </div>
+                    </div>
+                    <div className='arena-desktop-container'>
+
+                    </div>
+                    <div className='stats-container'>
+
+                    </div>
+                </div>
+                <div className='arena-container-2'>
+                    <div className='arena-mobile-container'>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
