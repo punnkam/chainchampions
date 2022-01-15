010ad26ccc0b564500d9a31d0f1e67c829362748
CodingCocho <raulujr@gmail.com>
2022-01-14 17:22:33 -0800

    Fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,38 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import './Navbar.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
-
-const Container = styled.div`
-	width: 100%;
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	justify-content: center;
-`;
-
-const StyledLink = styled(Link)`
-	color: black;
-	font-size: 1rem;
-	// margin: 0.5rem;
-	// margin-right: 3rem;
-	&:hover {
-		text-decoration: none;
-		color: grey;
-	}
-`;
-
-const Title = styled(Link)`
-	color: black;
-	font-size: 1.7rem;
-	// margin-left: 2rem;
-	// margin-right: 4rem;
-	&:hover {
-		text-decoration: none;
-		color: black;
-	}
-`;
 
 const Divider = styled.hr`
 	border-top: 3px dashed black;
@@ -44,16 +12,15 @@ const Divider = styled.hr`
 export default function Navbar() {
 	return (
 		<div className='Navbar'>
-			<Container>
-				<Title to='/'>Chain Champions</Title>
-				<FontAwesomeIcon icon={faBars} className='menu-icon'></FontAwesomeIcon>
+			<div className='container'>
+				<Link className='title-link' to='/'>Chain Champions</Link>
 				<div className='link-container'>
-					<StyledLink to='/'>Home</StyledLink>
-					<StyledLink to='/'>Arena</StyledLink>
-					<StyledLink to='/'>Whitepaper</StyledLink>
-					<StyledLink to='/'>Staking</StyledLink>
+					<Link className='link' to='/'>Home</Link>
+					<Link className='link' to='/'>Arena</Link>
+					<Link className='link' to='/'>Whitepaper</Link>
+					<Link className='link' to='/'>Staking</Link>
 				</div>
-			</Container>
+			</div>
 			<Divider />
 		</div>
 		
