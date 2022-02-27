76d4697a5bb73d0a079dfffde4d7b86df75abf74
Punn Kamolyabutr <punn.kamolyabutr@gmail.com>
2022-02-26 22:36:16 -0800

    constructor and testmethod

--- hardhat/contracts/Arena.sol
+++ hardhat/contracts/Arena.sol
@@ -40,13 +40,25 @@ contract Arena is IERC721Receiver, Ownable, Pausable {
         _;
     }
 
-    constructor() {
-
+    /// @notice Sets all contract state to inactive
+    /// @param _arenaActive The state of the arena, active or inactive
+    /// @param _contractActive The state of the contract, active or inactive
+    constructor(bool _arenaActive, bool _contractActive) {
+        arenaActive = _arenaActive;
+        contractActive = _contractActive;
     }
 
     /*******************
     *** VIEW METHODS ***  
     *******************/
+    
+    /// @notice Returns the tick spacing for a given fee amount, if enabled, or 0 if not enabled
+    /// @dev A fee amount can never be removed, so this value should be hard coded or cached in the calling context
+    /// @param fee The enabled fee, denominated in hundredths of a bip. Returns 0 in case of unenabled fee
+    /// @return The tick spacing
+    function testFunction(address fee) public returns(uint256) {
+
+    }
 
     /*******************
     *** USER METHODS ***  
@@ -62,8 +74,4 @@ contract Arena is IERC721Receiver, Ownable, Pausable {
     * INTERNAL METHODS *  
     *******************/
 
-
-    function test() public {
-        return 0;
-    }
 }
